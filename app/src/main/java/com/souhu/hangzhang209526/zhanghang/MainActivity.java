eac3bd3bbf72e2167f771558d529d5164ee2a424
hotpotboy <407376249@qq.com>
2015-08-28 17:33:23 +0800

    LineraLayout

--- app/src/main/java/com/souhu/hangzhang209526/zhanghang/MainActivity.java
+++ app/src/main/java/com/souhu/hangzhang209526/zhanghang/MainActivity.java
@@ -3,8 +3,14 @@ package com.souhu.hangzhang209526.zhanghang;
 import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
@@ -14,9 +20,11 @@ import com.android.volley.toolbox.NetworkImageView;
 import com.android.volley.toolbox.Volley;
 import com.souhu.hangzhang209526.zhanghang.adpter.ImageAdapter;
 import com.souhu.hangzhang209526.zhanghang.utils.ImageCache;
+import com.souhu.hangzhang209526.zhanghang.widget.HorizontalListView;
 import com.souhu.hangzhang209526.zhanghang.widget.ZhLinearLayout;
 import com.souhu.hangzhang209526.zhanghang.widget.ZhListView;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 
 import static com.souhu.hangzhang209526.zhanghang.adpter.ImageAdapter.*;
@@ -28,6 +36,7 @@ public class MainActivity extends Activity {
     ImageAdapter adapter;
     private static ArrayList<String> list;
     LinearLayout linearLayout;
+    private ZhListView mListView;
     static{
         list = new ArrayList<>();
         list.add("http://p3.qhimg.com/t0167a9dbb748dbff77.jpg");
@@ -40,15 +49,50 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
 //        imageView = (NetworkImageView) findViewById(R.id.net_work_image);
 //        //初始化图片加载器
 //        ImageLoader imageLoader = new ImageLoader(Volley.newRequestQueue(this), new ImageCache(8*1024*1024));
 //        imageView.setDefaultImageResId(R.drawable.deafult);
 //        imageView.setImageUrl("http://p3.qhimg.com/t0167a9dbb748dbff77.jpg11", imageLoader);
-        imageList = (ZhLinearLayout)findViewById(R.id.imageList);
+//        imageList = (ZhLinearLayout)findViewById(R.id.imageList);
 //        adapter = new ImageAdapter(this,list);
 //        imageList.setAdapter(adapter);
+        String[] data = {"标题1","标题2","标题3","标题4","标题5","标题6","标题7","标题1","标题2","标题3","标题4","标题5","标题6","标题7"};
+        ArrayAdapter adapter = new ArrayAdapter(getBaseContext(),R.layout.layout_item1,R.id.title,data);
+
+        //HorizontalListView测试
+        HorizontalListView listView = new HorizontalListView(getBaseContext());
+        setContentView(listView,new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        listView.setAdapter(adapter);
+
+        //ZhListView测试
+//        mListView = new ZhListView(getBaseContext());
+//        setContentView(mListView,new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+//        mListView.setAdapter(adapter);
+//        mListView.setOnTouchListener(new View.OnTouchListener() {
+//            @Override
+//            public boolean onTouch(View v, MotionEvent event) {
+////                lookTouchMode();
+//                if(event.getAction()==MotionEvent.ACTION_UP){
+//                    mListView.requestLayout();
+//                }
+//                return false;
+//            }
+//        });
+    }
+
+    private void lookTouchMode(){
+        try {
+            Field filed = AbsListView.class.getDeclaredField("mTouchMode");
+            filed.setAccessible(true);
+            int value = filed.getInt(mListView);
+//            Log.e("zhanghang",value+"");
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
